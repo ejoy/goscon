b54d7c65945868e294c652b52ba17b756ae151ed
jietao.cjt <jietao.cjt@alibaba-inc.com>
2018-12-26 16:45:15 +0800

    benchmark support multiple connections

--- examples/bench/main.go
+++ examples/bench/main.go
@@ -15,6 +15,13 @@ import (
 	"github.com/xtaci/kcp-go"
 )
 
+type Stat struct {
+	conn     int
+	slow     int
+	round    int
+	percent  map[int]int
+}
+
 func DialWithOptions(network, connect string, fecData, fecParity int) (net.Conn, error) {
 	if network == "tcp" {
 		return net.Dial(network, connect)
@@ -33,7 +40,7 @@ func DialScon(network, connect string, targetServer string, fecData, fecParity i
 	return scon, nil
 }
 
-func bench(conn net.Conn, host string, payload string) {
+func bench(i int, conn net.Conn, host string, payload string, chStat chan Stat) {
 	req, err := http.NewRequest("GET", "http://"+host+"/", nil)
 	if err != nil {
 		panic(err)
@@ -46,8 +53,12 @@ func bench(conn net.Conn, host string, payload string) {
 
 	writer := bufio.NewWriter(conn)
 
-	round := 0
-	slow := 0
+	stat := Stat{conn: i, slow: 0, round: 0, percent: make(map[int]int)}
+	interval := []int{1, 10, 100, 200, 0xFFFFFFFF}
+	for _, bound := range interval {
+		stat.percent[bound] = 0
+	}
+
 	for range ticker.C {
 		start := time.Now()
 
@@ -73,29 +84,35 @@ func bench(conn net.Conn, host string, payload string) {
 
 		elapsed := time.Now().Sub(start)
 
-		round++
-		if elapsed > time.Millisecond*10 {
-			slow++
+		stat.round++
+		if elapsed > 10 * time.Millisecond {
+			stat.slow++
 		}
-
-		if round%100 == 0 {
-			log.Printf("Round %d, slow: %d", round, slow)
+		for _, bound := range interval {
+			if elapsed <= time.Duration(bound) * time.Millisecond {
+				stat.percent[bound]++
+				break
+			}
+		}
+		if stat.round % 100 == 0 {
+			chStat <- stat
 		}
 	}
 }
 
 func main() {
 	var connect, targetServer string
+	var connections int
+	var payload int
 	var fecData int
 	var fecParity int
 	var network string
-	var payload int
 	flag.StringVar(&connect, "connect", "127.0.0.1:1248", "connect to")
 	flag.StringVar(&targetServer, "targetServer", "", "target server")
+	flag.IntVar(&connections, "connections", 1, "concurrent connections")
 	kcp := flag.NewFlagSet("kcp", flag.ExitOnError)
 	kcp.IntVar(&fecData, "fec_data", 1, "FEC: number of shards to split the data into")
 	kcp.IntVar(&fecParity, "fec_parity", 0, "FEC: number of parity shards")
-
 	flag.IntVar(&payload, "payload", 0, "http test payload size")
 	flag.Parse()
 
@@ -107,10 +124,21 @@ func main() {
 		network = "tcp"
 	}
 
-	sconn, err := DialScon(network, connect, targetServer, fecData, fecParity)
-	if err != nil {
-		log.Fatal(err)
+	chStat := make(chan Stat, 4*connections)
+	for i := 0; i < connections; i++ {
+		idx := i + 1
+		go func() {
+			sconn, err := DialScon(network, connect, targetServer, fecData, fecParity)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bench(idx, sconn, connect, strings.Repeat("a", payload), chStat)
+		}()
 	}
 
-	bench(sconn, connect, strings.Repeat("a", payload))
+	for stat := range chStat {
+		log.Printf("======== stat result on conn: %d", stat.conn)
+		log.Printf(">>>> slow: %d, round: %d, slow rate: %.3f", stat.slow, stat.round, float32(stat.slow)/float32(stat.round))
+		log.Printf(">>>> distribution: (0, 1]: %d, (1, 10]: %d, (10, 100]: %d, (100, 200]: %d, (200, inf]: %d", stat.percent[1], stat.percent[10], stat.percent[100], stat.percent[200], stat.percent[0xFFFFFFFF])
+	}
 }
