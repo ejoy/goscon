dfb035ca18a6780a8e0dd91a725363123a6dd648
jietao.cjt <jietao.cjt@alibaba-inc.com>
2018-12-28 10:45:52 +0800

    增加kcp参数

--- examples/bench/main.go
+++ examples/bench/main.go
@@ -9,8 +9,6 @@ import (
 	"strings"
 	"time"
 
-	//"os"
-	//"fmt"
 	"github.com/ejoy/goscon/scp"
 	"github.com/xtaci/kcp-go"
 )
@@ -25,16 +23,33 @@ type Stat struct {
 var statGap int
 var statLevel int
 
-func DialWithOptions(network, connect string, fecData, fecParity int) (net.Conn, error) {
+var (
+	kcpSndWnd    int
+	kcpRcvWnd    int
+	kcpNodelay   int
+	kcpInterval  int
+	kcpResend    int
+	kcpNc        int
+	kcpFecData   int
+	kcpFecParity int
+)
+
+func DialWithOptions(network, connect string) (net.Conn, error) {
 	if network == "tcp" {
 		return net.Dial(network, connect)
 	} else {
-		return kcp.DialWithOptions(connect, nil, fecData, fecParity)
+		conn, err := kcp.DialWithOptions(connect, nil, kcpFecData, kcpFecParity)
+		if err != nil {
+			return nil, err
+		}
+		conn.SetNoDelay(kcpNodelay, kcpInterval, kcpResend, kcpNc)
+		conn.SetWindowSize(kcpSndWnd, kcpRcvWnd)
+		return conn, nil
 	}
 }
 
-func DialScon(network, connect string, targetServer string, fecData, fecParity int) (*scp.Conn, error) {
-	conn, err := DialWithOptions(network, connect, fecData, fecParity)
+func DialScon(network, connect string, targetServer string) (*scp.Conn, error) {
+	conn, err := DialWithOptions(network, connect)
 	if err != nil {
 		return nil, err
 	}
@@ -104,19 +119,24 @@ func main() {
 	var connect, targetServer string
 	var connections int
 	var payload int
-	var fecData int
-	var fecParity int
 	var network string
 	flag.StringVar(&connect, "connect", "127.0.0.1:1248", "connect to")
 	flag.StringVar(&targetServer, "targetServer", "", "target server")
 	flag.IntVar(&connections, "connections", 1, "concurrent connections")
 	flag.IntVar(&statGap, "statGap", 10, "stat milliseconds per gap")
 	flag.IntVar(&statLevel, "statLevel", 10, "stat level")
-	kcp := flag.NewFlagSet("kcp", flag.ExitOnError)
-	kcp.IntVar(&fecData, "fec_data", 1, "FEC: number of shards to split the data into")
-	kcp.IntVar(&fecParity, "fec_parity", 0, "FEC: number of parity shards")
 	flag.IntVar(&payload, "payload", 0, "http test payload size")
 	flag.Parse()
+
+	kcp := flag.NewFlagSet("kcp", flag.ExitOnError)
+	kcp.IntVar(&kcpSndWnd, "sndWnd", 1024, "send window")
+	kcp.IntVar(&kcpRcvWnd, "rcvWnd", 1024, "recv window")
+	kcp.IntVar(&kcpNodelay, "nodelay", 1, "nodelay")
+	kcp.IntVar(&kcpInterval, "interval", 10, "milliseconds for update")
+	kcp.IntVar(&kcpResend, "resend", 2, "resend")
+	kcp.IntVar(&kcpNc, "nc", 1, "congestion control")
+	kcp.IntVar(&kcpFecData, "fecData", 0, "FEC: number of shards to split the data into")
+	kcp.IntVar(&kcpFecParity, "fecParity", 0, "FEC: number of parity shards")
 
 	args := flag.Args()
 	if len(args) > 0 && args[0] == "kcp" {
@@ -138,7 +158,7 @@ func main() {
 	for i := 0; i < connections; i++ {
 		idx := i + 1
 		go func() {
-			sconn, err := DialScon(network, connect, targetServer, fecData, fecParity)
+			sconn, err := DialScon(network, connect, targetServer)
 			if err != nil {
 				log.Fatal(err)
 			}
