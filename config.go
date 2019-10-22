fd81a0724202fff78e62c19131879f796840b98b
xjdrew <xj.drew@gmail.com>
2019-10-22 17:16:21 +0800

    improve readme

--- config.go
+++ config.go
@@ -24,21 +24,21 @@ func init() {
 	viper.SetDefault("tcp_option.keepalive", true)        // tcp keepalive: true
 	viper.SetDefault("tcp_option.keepalive_interval", 60) // tcp keepalive_interval: 60s
 
-	viper.SetDefault("kcp_option.reuseport", runtime.NumCPU()) // kcp reuseport: count of cpu
-	viper.SetDefault("kcp_option.read_timeout", 60)            // kcp read_timeout: 60s
+	viper.SetDefault("kcp_option.reuseport", runtime.NumCPU()) // kcp reuseport: count of cpu, 利用端口复用的特性，同时开启多个 Goroutine 监听端口，默认为cpu核数
+	viper.SetDefault("kcp_option.read_timeout", 60)            // kcp read_timeout: 60s, 接收数据超时时间，超时会关闭客户端对应连接
 	viper.SetDefault("kcp_option.fec_data_shards", 0)          // kcp fec, disable, refer to: https://www.backblaze.com/blog/reed-solomon/
 	viper.SetDefault("kcp_option.fec_parity_shards", 0)
-	viper.SetDefault("kcp_option.read_buffer", 4194304)  // kcp read_buffer: 4M
-	viper.SetDefault("kcp_option.write_buffer", 4194304) // kcp write_buffer: 4M
+	viper.SetDefault("kcp_option.read_buffer", 4194304)  // kcp read_buffer: 4M, udp socket 的 RCV_BUF，单位为字节，默认为 4M
+	viper.SetDefault("kcp_option.write_buffer", 4194304) // kcp write_buffer: 4M, udp socket 的 SND_BUF，单位为字节，默认为 4M
 
 	// kcp protocol configure, refer to: https://github.com/skywind3000/kcp/blob/master/README.en.md#protocol-configuration
-	viper.SetDefault("kcp_option.opt_mtu", 1400)    // kcp mtu: 1400
-	viper.SetDefault("kcp_option.opt_nodelay", 1)   // kcp opt_nodelay: 1
-	viper.SetDefault("kcp_option.opt_interval", 10) // kcp opt_interval: 10ms
-	viper.SetDefault("kcp_option.opt_resend", 2)    // kcp opt_interval: 2
-	viper.SetDefault("kcp_option.opt_nc", 1)        // kcp opt_nc: 1
-	viper.SetDefault("kcp_option.opt_sndwnd", 2048) // kcp opt_sndwnd: 2048 byte
-	viper.SetDefault("kcp_option.opt_rcvwnd", 2048) // kcp opt_sndwnd: 2048 byte
+	viper.SetDefault("kcp_option.opt_mtu", 1400)    // kcp mtu: 1400, 最大传输单元
+	viper.SetDefault("kcp_option.opt_nodelay", 1)   // kcp opt_nodelay: 1, 是否启用 nodelay 模式
+	viper.SetDefault("kcp_option.opt_interval", 10) // kcp opt_interval: 10ms, kcp 调用 update 的时间间隔，单位是毫秒
+	viper.SetDefault("kcp_option.opt_resend", 2)    // kcp opt_interval: 2,  快速重换模式，比如 2 表示：2 次 ACK 跨越直接重传
+	viper.SetDefault("kcp_option.opt_nc", 1)        // kcp opt_nc: 1, 是否关闭拥塞控制，0-开启，1-关闭
+	viper.SetDefault("kcp_option.opt_sndwnd", 2048) // kcp opt_sndwnd: 2048 byte, kcp连接的发送窗口
+	viper.SetDefault("kcp_option.opt_rcvwnd", 2048) // kcp opt_sndwnd: 2048 byte, kcp连接的接收窗口
 }
 
 func configItemTime(name string) time.Duration {
---
+++ protocol.md
@@ -0,0 +1,85 @@
+# Stable Connection Protocol
+## 介绍
+为了解决移动场景下，网络闪断问题而订立的复用连接协议。
+
+## 协议
+
+### 新建连接
+
+Client->Server: 传输一个 2 byte size(big-endian) + content 的包, size == len(content)
+
+包的内容如下:
+
+```
+0\n
+base64(DHPublicKey)\n
+targetServer
+```
+
+DHPublicKey 是一个 8 bytes 值, 经过 DH 算法计算出来的 key。
+
+`targetServer`用于提示优先连接的后端服务器名字。`targetServer`应该仅包括[a-zA-Z_0-9]。
+
+```
+DHPrivateKey = dh64.PrivateKey()
+DHPublicKey = dh64.PublicKey(DHPrivateKey)
+```
+
+Server->Client: 回应给 Client 一个握手信息:
+
+```
+id\n
+base64(DHPublicKey)
+```
+
+这里, id 是一个 10 进制的非 0 数字串. 建议在 [1,2^32) 之间. 因为实现可能利用 uint32_t 保存这个 id .
+
+DHPublicKey 的算法同 client 的算法.
+
+握手完毕后, 双方获得一个公有的 64bit secret,  计算方法为:
+
+```
+secret = dh64.Secret(myDHPrivateKey, otherDHPublicKey)
+```
+
+### 恢复连接
+
+Client->Server: 传输一个 2 byte size(big-endian) + content 的包, size == len(content)
+
+包的内容如下:
+
+```
+id\n
+index\n
+recvnumber\n
+base64(HMAC_CODE)
+```
+
+这里 id 为新建连接时, 服务器交给 Client 的 id .
+
+index 是一个从 1 开始(第一次恢复为 1), 递增的十进制数字. 服务器会拒绝重复使用过的数字.
+
+recvnumber 是一个 10 进制数字, 表示 (曾经从服务器收到多少字节 mod 2^32).
+
+把以上三行放在一起(保留 \n) content, 以及在新建连接时交换得到的 serect, 计算一个 HMAC_CODE, 算法是:
+
+HMAC_CODE = crypt.hmac64(crypt.hashkey(content), secret)
+
+Server->Client: 回应握手消息:
+
+```
+recvnumber\n
+CODE msg
+```
+
+这里, recvnumber 是一个 10 进制数字, 表示 (曾经在这个会话上, 服务器收到过客户端发出的多少字节 mod 2^32).
+CODE 是一个10进制三位数, 表示连接是否恢复:
+
+* 200 OK : 表示连接成功
+* 401 Unauthorized : 表示 HMAC 计算错误
+* 403 Index Expired : 表示 Index 已经使用过
+* 404 User Not Found : 表示连接 id 已经无效
+* 406 Not Acceptable : 表示 cache 的数据流不够
+
+当连接恢复后, 服务器应当根据之前记录的发送出去的字节数（不计算每次握手包的字节）, 减去客户端通知它收到的字节数, 开始补发未收到的字节。
+客户端也做相同的事情。
