b3f3c5238d4c597219b7b37a8ec2214dc360aefa
xjdrew <xj.drew@gmail.com>
2019-10-22 16:48:35 +0800

    use glog

---
+++ util.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"runtime"
+)
+
+// stacks is a wrapper for runtime.Stack that attempts to recover the data for all goroutines.
+func stacks(all bool) []byte {
+	// We don't know how big the traces are, so grow a few times if they don't fit. Start large, though.
+	n := 10000
+	if all {
+		n = 100000
+	}
+	var trace []byte
+	for i := 0; i < 5; i++ {
+		trace = make([]byte, n)
+		nbytes := runtime.Stack(trace, all)
+		if nbytes < len(trace) {
+			return trace[:nbytes]
+		}
+		n *= 2
+	}
+	return trace
+}
